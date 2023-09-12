3b82ce6860609504a127db327e79169bb7af4f82
Egbe Alexander <alexsucceeds@gmail.com>
2023-09-12 05:35:28 +0100

    improve route

--- route.js
+++ route.js
@@ -3,7 +3,7 @@ const validate = require("./validation")
 const {createUser, getUser, updateUser, deleteUser} = require("./controller/user.controller")
 
 
-router.route("/persons").get(getUser).post(validate, createUser)
-router.route("/persons/:id").put(updateUser).delete(deleteUser)
+router.route("").get(getUser).post(validate, createUser)
+router.route("/:id").put(updateUser).delete(deleteUser)
 
 module.exports = router
