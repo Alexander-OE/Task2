f778db663a08e4ec7e83e41b3a18bb138a160df7
Egbe Alexander <alexsucceeds@gmail.com>
2023-09-13 14:53:29 +0100

    feat: improved dynamic fetching functionality

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,26 +1,121 @@
 const User = require("../model/user");
+const mongoose = require("mongoose");
 
-const getUser = async (req, res) => {
+
+
+const getAllUser = async (req, res) => {
   const user = await User.find();
   res.status(200).json({ user });
 };
 
+
+
 const createUser = async (req, res) => {
-  const newUser = new User(req.body);
-  await newUser.save();
-  res.status(201).json({ msg: "User Created", data: newUser });
+  try {
+    // Check if a user with the same name already exists
+    const existingUser = await User.findOne({ name: req.body.name });
+
+    if (existingUser) {
+      return res
+        .status(409)
+        .json({ error: "User with this name already exists" });
+    }
+
+    // If no user with the same name exists, create and save the new user
+    const newUser = new User(req.body);
+    await newUser.save();
+
+    res.status(201).json({ user: newUser });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
+
+
+
+
+const getUser = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    let user;
+
+    // Check if the idOrName parameter is a valid ObjectId
+    if (mongoose.Types.ObjectId.isValid(id)) {
+      user = await User.findById(id);
+    } else {
+      // If it's not a valid ObjectId, assume it's a name
+      user = await User.findOne({ name: id });
+    }
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    res.status(200).json({ user });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+
 
 const updateUser = async (req, res) => {
   const { id } = req.params;
-  const currentUser = await User.findByIdAndUpdate(id, req.body, { new: true });
-  res.status(200).json({ currentUser });
+  const searchCriteria = {};
+
+  // Check if id is a valid ObjectId; if so, use it to search by _id
+  if (mongoose.Types.ObjectId.isValid(id)) {
+    searchCriteria._id = id;
+  } else {
+    // If not a valid ObjectId, assume it's a name and search by name
+    searchCriteria.name = id;
+  }
+
+  try {
+    const currentUser = await User.findOneAndUpdate(searchCriteria, req.body, {
+      new: true,
+    });
+
+    if (!currentUser) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    res.status(200).json({ currentUser });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
+
+
+
 
 const deleteUser = async (req, res) => {
   const { id } = req.params;
-  await User.findByIdAndDelete(id);
-  res.status(204).end();
+
+  try {
+    let user;
+
+    // Check if the id parameter is a valid ObjectId
+    if (mongoose.Types.ObjectId.isValid(id)) {
+      user = await User.findByIdAndDelete(id);
+    } else {
+      // If it's not a valid ObjectId, assume it's a name
+      user = await User.findOneAndDelete({ name: id });
+    }
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    res.status(204).end();
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
 
-module.exports = { createUser, getUser, updateUser, deleteUser };
+module.exports = { createUser, getAllUser, updateUser, deleteUser, getUser };
--- route.js
+++ route.js
@@ -1,9 +1,9 @@
 const router = require("express").Router()
 const validate = require("./validation")
-const {createUser, getUser, updateUser, deleteUser} = require("./controller/user.controller")
+const {createUser, getAllUser, updateUser, deleteUser, getUser} = require("./controller/user.controller")
 
 
-router.route("").get(getUser).post(validate, createUser)
-router.route("/:id").put(updateUser).delete(deleteUser)
+router.route("/").get(getAllUser).post(validate, createUser)
+router.route("/:id").put(updateUser).delete(deleteUser).get(getUser)
 
 module.exports = router
